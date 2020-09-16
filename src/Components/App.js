fd5fa03e0b54e7f28ab957a898f1213450f7ab76
aminsoltaninia <aminsoltani.nia@gmail.com>
2020-09-17 00:34:19 +0430

    add toggle and deletetodo

--- src/Components/App.js
+++ src/Components/App.js
@@ -33,7 +33,29 @@ class App extends Component {
         })
     }
 
+    deleteTodo(key){
+        this.setState(prevState=>{
+            return {
+                todos : prevState.todos.filter(item=> item.key !== key)
+            }
+        })
+    }  
+    
+    toggleTodo(key){
+        let {todos} = this.state
 
+        let item = todos.find(item => item.key === key )
+
+        item.done =! item.done ;
+        let newTodods = todos.filter(item => item.key !== key)
+        this.setState({
+            todos : [
+                ...newTodods ,
+                 item
+            ]
+        })
+    }
+     
     render() {
 
         let {todos , statusDone }= this.state;
@@ -59,17 +81,23 @@ class App extends Component {
                                 <nav className="col-6 mb-3">
                                     <div className="nav nav-tabs" id="nav-tab" role="tablist">
 
-                                        <a className={`nav-item nav-link  font-weight-bold ${statusDone=== false ? 'active' : ''}`} 
+                                        <a className={`nav-item nav-link  font-weight-bold ${statusDone=== false ? 'active' :''}`} 
                                         id="nav-home-tab" onClick={()=>this.setState({statusDone : false})}>undone <span className="badge badge-secondary">{todos.filter(item=> item.done==false).length}</span></a>
 
-                                        <a className={`nav-item nav-link  font-weight-bold ${statusDone === true ? 'active' : ''}`} 
+                                        <a className={`nav-item nav-link  font-weight-bold ${statusDone === true ? 'active' :''}`} 
                                         id="nav-profile-tab" onClick={()=>this.setState({statusDone :true})}>done <span className="badge badge-success">{todos.filter(item=> item.done==true).length}</span></a>
 
                                     </div>
                                 </nav>
                             
                             {
-                                filterTodos.length === 0 ? <p className="col-6 mb-3 ">there isnt any todo</p> : filterTodos.map(item => <Todo key={item.key} text={item.text} />)
+                                filterTodos.length === 0 ?
+                                                         <p className="col-6 mb-3 ">there isnt any todo</p> :
+                                                          filterTodos.map(item => <Todo key={item.key}
+                                                                                        text={item}
+                                                                                        deleteTodo={this.deleteTodo.bind(this)} 
+                                                                                        done = {this.toggleTodo.bind(this)}
+                                                                                        />)
                             }
                             </div>
                         </div>
--- src/Components/todo.js
+++ src/Components/todo.js
@@ -3,16 +3,24 @@ import React from 'react';
 
 function Todo(props) {
 
+    console.log(props)
     return (
 
-        <div className="col-6 mb-3 ">
+        <div className="col-6 mb-3">
             <div className="d-flex justify-content-between align-items-center border rounded p-3">
                 <div>
-                   {props.text}
+                   {props.text.text}
             </div>
                 <div>
-                    <button type="button" className="btn btn-info btn-sm">edit</button>
-                    <button type="button" className="btn btn-danger btn-sm ml-1">delete</button>
+                     <button type="button"
+                             className="btn btn-success btn-sm mr-1" 
+                             onClick={()=>{props.done(props.text.key)}}
+    >{props.text.done ? 'undone' : 'done'}</button>
+                    <button type="button" className="btn btn-info btn-sm mr-1">edit</button>
+                    <button type="button" 
+                            className="btn btn-danger btn-sm " 
+                            onClick={()=>{props.deleteTodo(props.text.key)}}
+                    >delete</button>
                 </div>
             </div>
         </div>
